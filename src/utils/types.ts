7e2232431ff93dc21fcbe238e4e927a5ed2b175d
martadzielak <marta.dzielak@gmail.com>
2021-08-19 19:04:02 +0200

    Updates

--- src/utils/types.ts
+++
@@ -1,7 +0,0 @@
-export interface IItem {
-  link: string | undefined;
-  title: string | undefined;
-  pubDate: number | undefined;
-  creator: string | undefined;
-  description: string | undefined;
-}
