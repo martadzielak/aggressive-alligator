4f38eecccd634a8d91f7c53fd07df6e0a83233f7
martadzielak <marta.dzielak@gmail.com>
2022-03-29 17:12:27 +0200

    feat: change to fetch method

--- src/utils/getFeedItemsArray.ts
+++ src/utils/getFeedItemsArray.ts
@@ -4,14 +4,10 @@ import { parseDate, sortChronologically } from "./dateHelpers";
 export const getFeedItemsArray = async () => {
   const feedItemsArray = [];
   for (const feedUrl of feedUrls) {
-    const request = new XMLHttpRequest();
-    request.open("GET", feedUrl, false);
-    request.send();
-    const responseText = request.responseText;
-    const contentsParsed = new window.DOMParser().parseFromString(
-      responseText,
-      "text/xml"
-    );
+    const contentsParsed = await fetch(feedUrl)
+      .then((response) => response.text())
+      .then((str) => new window.DOMParser().parseFromString(str, "text/xml"));
+
     const selectedFeeds = contentsParsed.querySelectorAll("item");
     const feedItems = [...selectedFeeds].map((el) => ({
       link: el.querySelector("link")?.innerHTML,
